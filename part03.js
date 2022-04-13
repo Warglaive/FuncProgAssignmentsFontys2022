e245e739ce3b7303698c483ef41f8ead33a77bb2
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-13 18:30:47 +0200

    Update part03.js
    wwwww

--- part03.js
+++ part03.js
@@ -16,44 +16,44 @@ const Left = x => ({
 
 const Right = x => ({
     flatMap: r => r(x),
-    emit: undefined,
-    map: undefined,
-    isLeft: undefined,
-    isRight: undefined,
+    emit: () => x,
+    map: r => Either.of(r(x)),
+    isLeft: false,
+    isRight: true,
     inspect: () => `Right(${x})`
 });
 
 const Either = {
-    of: undefined
+    of: x => x instanceof Error || x.isLeft ? Left(x) : Right(x)
 };
 
 // Simulate saving to the database and return the result
 // If val is a string the operation is successful and a Right is returned
 // If val is not of type string the operation fails and a Left is returned
-// const saveToDataBase = (val) => {
-//     if(typeof val === 'string'){
-//         return Right(val);
-//     }
-//     return Left(new Error('Failed to save to database, incorrect type'));
-// }
+const saveToDataBase = (val) => {
+    if(typeof val === 'string'){
+        return Right(val);
+    }
+    return Left(new Error('Failed to save to database, incorrect type'));
+}
 
 // Simulate map/flatMap that goes right
-// const success = Either.of('test')
-//     .map(x => x.toUpperCase())
-//     .flatMap(saveToDataBase)
-//     .map(x => x + ' saved')
-//     .emit();
-// Should log: 'TEST saved'
-// console.log(success);
+const success = Either.of('test')
+    .map(x => x.toUpperCase())
+    .flatMap(saveToDataBase)
+    .map(x => x + ' saved')
+    .emit();
+//Should log: 'TEST saved'
+console.log(success);
 
-// Simulate map/flatMap that goes wrong
-// const failure = Either.of(10)
-//     .map(x => x * 2)
-//     .flatMap(saveToDataBase)
-//     .map(x => x + ' saved')
-//     .emit()
+//Simulate map/flatMap that goes wrong
+const failure = Either.of(10)
+    .map(x => x * 2)
+    .flatMap(saveToDataBase)
+    .map(x => x + ' saved')
+    .emit()
 // Should log true
-// console.log(failure instanceof Error);
+ console.log(failure instanceof Error);
 
 module.exports = {
     Left,
