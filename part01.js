54bb3b86c2dd80b3a3ef5a2013ec74dc020e40c4
Svetoslav Stoyanov <426911@student.fontys.nl>
2022-04-13 16:47:05 +0200

    Update part01.js

--- part01.js
+++ part01.js
@@ -3,10 +3,10 @@ const square = x => x * x;
 
 // Compose the two fuctions, such that it becomes a new function
 // that first squares and then doubles
-const squareAndDouble = x => undefined;
+const squareAndDouble = x => double(square(x));
 
 // Use to test, answer should be: 50
-//console.log(squareAndDouble(5));
+console.log(squareAndDouble(5));
 
 // Create a higher order function that does the composition for you
 // Assume we only need to pass one argument to the first function
